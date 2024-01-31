d0572e11b68de0d7794d26fbc0e8b985ca0c5753
Sohum Chandra Srivastava <146676749+sohumcs@users.noreply.github.com>
2024-02-01 03:31:14 +0530

    Time: 418 ms (5.01%) | Memory: 45.5 MB (79.12%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int temp;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == 0) {
+                    temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
