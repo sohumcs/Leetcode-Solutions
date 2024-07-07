1116b7aa9af09a0577f080c9f4be31583471b181
Sohum Chandra Srivastava <146676749+sohumcs@users.noreply.github.com>
2024-07-08 02:58:20 +0530

    Time: 93 ms (76.40%) | Memory: 44.6 MB (56.40%) - LeetSync

---
+++ 447-number-of-boomerangs/number-of-boomerangs.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numberOfBoomerangs(int[][] points) {
+        HashMap<Integer, Integer> distances = new HashMap<>();
+        int boomerangs = 0;
+        for(int i = 0; i<points.length; i++){
+            for(int j = 0; j<points.length; j++){
+                if(i == j){
+                    continue;
+                }
+                int distance = (points[i][0] - points[j][0]) * (points[i][0] - points[j][0]) + (points[i][1] - points[j][1]) * (points[i][1] - points[j][1]);
+                distances.put(distance, distances.getOrDefault(distance,0) + 1);
+            }
+            for(Integer distance : distances.values()){
+                boomerangs += distance * (distance - 1);
+            }
+            distances.clear();
+        }
+        return boomerangs;
+    }
+}
